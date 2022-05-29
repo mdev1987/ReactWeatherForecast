7936c5f865a353ebd883f5e8eeefb405201e0ed0
mdeveloper66 <m.developer66@gmail.com>
2022-05-29 07:36:48 +0430

    Header component completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <ul className='flex ml-auto w-full font-bold'>
+      <i className="text-xs text-gray-800 
+                    ml-auto mr-6 border-b-2 
+                    border-green-400 
+                    cursor-pointer">Weather</i>
+      <i className="text-xs text-gray-800 
+                    mr-6 alert-notice 
+                    cursor-pointer border-b-2 
+                    hover:border-green-400">Alerts</i>
+      <i className="text-xs text-gray-800
+                    mr-6 cursor-pointer
+                    border-b-2 hover:border-green-400">Map</i>
+      <i className="text-xs text-gray-800
+                    mr-6 cursor-pointer border-b-2 
+                    hover:border-green-400">Satelite</i>
+      <i className="text-xs text-gray-800
+                    cursor-pointer border-b-2 
+                    hover:border-green-400">News</i>
+    </ul>
   )
 }
 
